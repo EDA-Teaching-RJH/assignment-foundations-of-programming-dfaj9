5cd174d06969df21276c7a97f7dc2c003ce880e5
dfaj9 <df339@kent.ac.uk>
2026-02-11 19:59:17 +0000

    commit2

--- fleet_manager.py
+++ fleet_manager.py
@@ -5,7 +5,26 @@ def init_database():
     ids = ["1", "2", "3", "4"]
     return n, r, d, ids
     
+
+def display_menu():
+    user_name = input("Enter your full name: ")
+    print("\n--- MENU ---")
+    print(user_name, "Has sucessfully logged in!")
+    print("1. Add member")
+    print("2. Remove member")
+    print("3. Update rank")
+    print("4. Display roster")
+    print("5. Search crew")
+    print("6. Filter by division")
+    print("7. Calculate payroll")
+    print("8. Count officers")
+    print("9. Exit")
+
+    option = input("Choose an option: ")
+    return option
 def main():
     n, r, d, ids = init_database()
     print(n, r, d, ids)
+    option = display_menu()
 main()
+
