7c8df1f8ef91d12abbdc0ba446c38da2761971ef
dfaj9 <df339@kent.ac.uk>
2026-02-11 19:18:43 +0000

    commit1

---
+++ fleet_manager.py
@@ -0,0 +1,6 @@
+def init_database():
+    n = ["Picard", "Riker", "Data", "Worf"]
+    r = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
+    d = ["Command", "Command", "Operations", "Security"]
+    ids = ["1", "2", "3", "4"]
+    return n, r, d, ids
